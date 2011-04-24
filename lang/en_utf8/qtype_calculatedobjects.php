3f30934eb50a11180f373f2bb22219f16a2e95ba
Nick Freear <nfreear@yahoo.co.uk>
2011-04-24 17:48:13 +0100

    CONTRIB-2888 lang/en_utf8/qtype_ca..: Adding Moodle 2 support.

--- lang/en_utf8/qtype_calculatedobjects.php
+++ lang/en_utf8/qtype_calculatedobjects.php
@@ -1,27 +1,14 @@
 <?php //$Id: qtype_calculatedobjects.php,v 1.2 2010/09/08 21:14:57 nfreear Exp $
+/**
+ * English (en) language pack for the Calculated Objects question type.
+ *
+ * @package   qtype_calculatedobjects
+ * @copyright (c) 2010 Nicholas Freear.
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v2 or later
+ *
+ * Moodle 1.9.x compatible.
+ */
+global $CFG;
+require_once($CFG->dirroot.
+    '/question/type/calculatedobjects/lang/en/qtype_calculatedobjects.php');
 
-// Language strings for creating/ editing questions.
-$string['addingcalculatedobjects'] = 'Adding a Calculated Objects question';
-$string['calculatedobjects']       = 'Calculated Objects';
-$string['editingcalculatedobjects']= 'Editing a Calculated Objects question';
-
-$string['mandatoryhdr'] = 'Mandatory wild cards present in answers';
-$string['possiblehdr']  = 'Possible wild cards present only in the question text';
-
-// The objects. I suggest we use plurals.
-$string['greenapple'] = //Synonyms/ aliases for 'apple'.
-$string['redapple']=
-$string['apple' ]  = 'apples';
-$string['orange']  = 'oranges';
-$string['pear'  ]  = 'pears';
-$string['pineapple']='pineappples';
-$string['cookie']  = 'cookies';
-$string['cup']     = 'cups';
-$string['coffee']  = 'cups of coffee';
-$string['walnut']  = 'walnuts';
-$string['tomato']  = 'tomatoes'; 
-$string['cake']    = 'cakes';
-$string['pencil']  = 'pencils';
-$string['car']     = 'cars';
-
-//End.
