8f56dad9ccfb6a1e69b5045ebcbd6725f4e7ff37
Nick Freear <nfreear@yahoo.co.uk>
2011-04-24 17:03:04 +0100

    lang/en/qtype..php: Adding Moodle 2 support.

---
+++ lang/en/qtype_calculatedobjects.php
@@ -0,0 +1,30 @@
+<?php //$Id: qtype_calculatedobjects.php,v 1.2 2010/09/08 21:14:57 nfreear Exp $
+
+// Language strings for creating/ editing questions.
+$string['addingcalculatedobjects'] = 'Adding a Calculated Objects question';
+$string['calculatedobjects']       = 'Calculated Objects';
+$string['calculatedobjectssummary']=
+$string['calculatedobjects_help']  =
+'A version of calculated questions which allows you to quickly set questions like "<em>How much is {apples} + {oranges}?</em>" They are like numerical questions but with the numbers used selected randomly from a set when the quiz is taken.';
+$string['editingcalculatedobjects']= 'Editing a Calculated Objects question';
+
+$string['mandatoryhdr'] = 'Mandatory wild cards present in answers';
+$string['possiblehdr']  = 'Possible wild cards present only in the question text';
+
+// The objects. I suggest we use plurals.
+$string['greenapple'] = //Synonyms/ aliases for 'apple'.
+$string['redapple']=
+$string['apple' ]  = 'apples';
+$string['orange']  = 'oranges';
+$string['pear'  ]  = 'pears';
+$string['pineapple']='pineappples';
+$string['cookie']  = 'cookies';
+$string['cup']     = 'cups';
+$string['coffee']  = 'cups of coffee';
+$string['walnut']  = 'walnuts';
+$string['tomato']  = 'tomatoes'; 
+$string['cake']    = 'cakes';
+$string['pencil']  = 'pencils';
+$string['car']     = 'cars';
+
+//End.
