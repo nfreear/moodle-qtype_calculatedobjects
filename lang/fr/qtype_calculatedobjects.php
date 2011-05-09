a19270f0ba8e6d6ff205822d893c00da4dcdd3e8
Nick Freear <nfreear@yahoo.co.uk>
2011-05-09 10:30:10 +0100

    CONTRIB-2924, initial commit of French language pack - Moodle 2 only for now.

---
+++ lang/fr/qtype_calculatedobjects.php
@@ -0,0 +1,36 @@
+<?php // $Id: qtype_calculated.php,v 1.15 2009/04/10 19:28:53 martignoni Exp $
+
+//Compare: http://cvs.moodle.org/lang/fr_utf8/qtype_calculated.php?view=markup
+
+// FR. Language strings for creating/ editing questions.
+$string['addingcalculatedobjects'] = 'Ajout d\'un objet calculée question';
+$string['calculatedobjects'] = 'Objets calculés';
+$string['editingcalculatedobjects'] = //'Modification d\'une question calculée';
+  'Modification d\'un objet calculée question';
+  //'Modifying an object derived question'!
+
+$string['mandatoryhdr'] = 'Caractère joker obligatoire présent dans les réponses';
+$string['possiblehdr'] = 'Des caractères joker sont peut-être présents seulement dans le texte de la question';
+
+// The objects. I suggest we use plurals.
+// Example, "I have 7 apples and 3 oranges..."
+// "J'ai 7 pommes et 3 oranges..."
+$string['greenapple'] = //Synonyms/ aliases for 'apple'.
+$string['redapple']=
+$string['apple' ]  = 'pommes';
+$string['orange']  = 'oranges';
+$string['pear'  ]  = 'poires';
+$string['pineapple']='ananas';
+$string['cookie']  = 'biscuits';
+$string['cup']     = 'tasses';
+$string['coffee']  = 'cups of coffee';
+$string['walnut']  = 'noix de Grenoble';
+$string['tomato']  = 'tomates'; 
+$string['cake']    = 'gâteaux';
+$string['pencil']  = 'crayons';
+$string['car']     = 'voitures';
+
+//http://translate.google.com/#en|fr|2%20apples.%203%20oranges.%204%20pears.%205%20pineapples.%206%20cookies.%207%20cups.%208%20walnuts.%209%20tomatoes.%2010%20cakes.%2011%20pencils.%2012%20cars.
+
+//End.
+
