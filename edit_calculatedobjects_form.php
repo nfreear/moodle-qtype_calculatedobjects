a6d59390f393ce61723cc65324a50a5dd27624f8
Nick Freear <nfreear@yahoo.co.uk>
2012-06-04 09:13:14 +0100

    Fixing comments and tidying

--- edit_calculatedobjects_form.php
+++ edit_calculatedobjects_form.php
@@ -5,8 +5,8 @@
  * @copyright &copy; 2007 Jamie Pratt
  * @author Jamie Pratt me@jamiep.org
  * @license http://www.gnu.org/copyleft/gpl.html GNU Public License
- * @package questionbank
- * @subpackage questiontypes
+ * @package    qtype
+ * @subpackage calculatedobjects
  */
 require_once("$CFG->dirroot/question/type/calculated/edit_calculated_form.php");
 
@@ -16,6 +16,5 @@ require_once("$CFG->dirroot/question/type/calculated/edit_calculated_form.php");
  */
 // Moodle 2.1+ compatible class names.
 class qtype_calculatedobjects_edit_form extends qtype_calculated_edit_form {
-//N: class question_edit_calculatedobjects_form extends question_edit_calculated_form { 
 }
 ?>
