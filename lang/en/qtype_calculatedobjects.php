b8d7110d4c7e507cf1e749c9ca6fbf4b4cacc58b
Nick Freear <nfreear@yahoo.co.uk>
2012-06-04 19:49:15 +0100

    Adding a list of supported objects to the Calculated Objects question editor popup

--- lang/en/qtype_calculatedobjects.php
+++ lang/en/qtype_calculatedobjects.php
@@ -1,13 +1,21 @@
 <?php //$Id: qtype_calculatedobjects.php,v 1.2 2010/09/08 21:14:57 nfreear Exp $
 
+require_once __DIR__ .'/../../renderer.php';
+
+
 $string['pluginname'] = 'Calculated Objects';
 
 // Language strings for creating/ editing questions.
 $string['addingcalculatedobjects'] = 'Adding a Calculated Objects question';
 $string['calculatedobjects']       = 'Calculated Objects';
-$string['calculatedobjectssummary']=
+// New question popup - restricted space.
+$string['calculatedobjectssummary']= $qco_summary =
+'An extension of calculated questions which allows you to quickly set questions like "<em>How much is {apples} + {oranges}?</em>" They are like numerical questions but with the numbers used selected randomly from a set when the quiz is taken.';
+// Help popup - more space.
 $string['calculatedobjects_help']  =
-'A version of calculated questions which allows you to quickly set questions like "<em>How much is {apples} + {oranges}?</em>" They are like numerical questions but with the numbers used selected randomly from a set when the quiz is taken.';
+  "<div class=qco-help>$qco_summary. Supported objects: <ul>"
+  . qtype_calculatedobjects_renderer::object_names_implode('<li>')
+  . ' </ul></div>';
 $string['editingcalculatedobjects']= 'Editing a Calculated Objects question';
 
 $string['mandatoryhdr'] = 'Mandatory wild cards present in answers';
