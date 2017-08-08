adbf5644620bb3196a55cded546e9dd379c76bef
germanvaleroelizondo <gvalero@unam.mx>
2017-08-08 14:11:56 -0500

    Add files via upload
    Mexican Spanish translation

---
+++ lang/es_mx/qtype_calculatedobjects.php
@@ -0,0 +1,50 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Local language pack from http://localhost
+ *
+ * @package    qtype
+ * @subpackage calculatedobjects
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['addingcalculatedobjects'] = 'Añadiendo una pregunta de Objetos Calculados';
+$string['apple'] = 'manzanas';
+$string['cake'] = 'pasteles';
+$string['calculatedobjects'] = 'Objetos Calculados';
+$string['calculatedobjects_help'] = '<div class=qco-help>Una extensión de las perguntas calculadas que le permite a Usted configurar rápidamente preguntas como por ejemplo "<em>¿Cuantas frutas son  {apples} + {oranges}?</em>" Son similares a las preguntas numéricas pero los números que usan son seleccionados aleatoriamente a partir de un conjunto cuando se hace el examen. Los objetos soportados son (anzanas, naranjas, peras, piñas, galletas, cafés, nueces, manzanas_rojas, manzanas_verdes, tomates, pasteles, carros, lápices): <ul>apple<li>orange<li>pear<li>pineapple<li>cookie<li>coffee<li>walnut<li>cup<li>redapple<li>greenapple<li>tomato<li>cake<li>car<li>pencil </ul></div>';
+$string['calculatedobjects_inputhint'] = 'Estoy esperando un número';
+$string['calculatedobjectssummary'] = 'Una extensión de las perguntas calculadas que le permite a Usted configurar rápidamente preguntas como por ejemplo "<em>¿Cuantas frutas son  {apples} + {oranges}?</em>" Son similares a las preguntas numéricas pero los números usados son seleccionados aleatoriamente de un conjunto cuando se hace el examen.';
+$string['car'] = 'carros';
+$string['coffee'] = 'tazas de café';
+$string['cookie'] = 'galletas';
+$string['cup'] = 'tazas';
+$string['editingcalculatedobjects'] = 'Editando una pregunta de Objetos Calculados';
+$string['greenapple'] = 'manzanas';
+$string['mandatoryhdr'] = 'Comodines obligatorios presentes en las respuestas';
+$string['orange'] = 'naranjas';
+$string['pear'] = 'peras';
+$string['pencil'] = 'lápices';
+$string['pineapple'] = 'piñas';
+$string['pluginname'] = 'Objetos Calculados';
+$string['possiblehdr'] = 'Comodines posibles presentes solamente en el texto de la pregunta';
+$string['redapple'] = 'manzanas';
+$string['tomato'] = 'tomates';
+$string['walnut'] = 'nueces';
